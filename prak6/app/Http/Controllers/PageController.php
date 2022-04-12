6dbc479fff5faf1ca8536f888f2655a611bb1134
Deliana <Delianabtr30@gmail.com>
2022-04-12 23:54:12 +0700

    semangat semangat

--- prak6/app/Http/Controllers/PageController.php
+++ prak6/app/Http/Controllers/PageController.php
@@ -1,6 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 
 class PageController extends Controller
 {
@@ -11,5 +14,11 @@ class PageController extends Controller
     public function tampil()
     {
         return "Data Mahasiswa";
+    }
+    public function cobaFacade()
+    {
+        echo Str::snake('SedangBerpuasaDiBulanRamadhan');
+        echo "<br>";
+        echo Str::kebab('SedangSahurSeorangDiri');
     }
 }
