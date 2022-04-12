cf76e6635d59170d2a3e4762915e9e3910a90cb7
Deliana <Delianabtr30@gmail.com>
2022-04-12 08:21:27 +0700

    tidak selesai hue hue

---
+++ prak6/routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('mahasiswa', function () {
+//     $arrMahasiswa = [
+//         "Luhut Panjaitan", "Jokowi", "Vladimir Putin",
+//         "Lisa Permata"
+//     ];
+//     return view('mahasiswa')->with('mahasiswa', $arrMahasiswa);
+// });
+
+// Route::get('dosen', function () {
+//     $arrDosen = [
+//         "Maya Fitrianti, M.M", "Prof. Silvia Nst, M.Kom.",
+//         "Dr. Umar Agustinus", "Dr. Syahrial, M.Kom"
+//     ];
+//     return view('dosen')->with('dosen', $arrDosen);
+// });
+
+// Route::get('gallery', function () {
+//     return view('gallery');
+// });
+
+
+Route::get('/home', [App\Http\Controllers\PageController::class, 'index']);
+Route::get('/mahasiswa', [App\Http\Controllers\PageController::class, 'tampil']);
